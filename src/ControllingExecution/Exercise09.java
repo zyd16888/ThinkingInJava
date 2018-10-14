901f8e3679940dfdcedbab5eaff1b9dd28dae197
zyd16888 <zyd475182659@163.com>
2018-10-14 21:46:26 +0800

    斐波那契数列

--- src/ControllingExecution/Exercise09.java
+++ src/ControllingExecution/Exercise09.java
@@ -1,5 +1,7 @@
 package ControllingExecution;
 
+import static java.lang.Integer.parseInt;
+
 /**
  * 描述:
  * 斐波那契数列
@@ -8,5 +10,20 @@ package ControllingExecution;
  * @date 2018-10-14 21:26
  */
 public class Exercise09 {
-    
+
+    static int fib(int n) {
+        if (n <= 2)
+            return 1;
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args) {
+        // 得到最大值从命令行：
+        int n = Integer.parseInt(args[0]);
+        if(n < 0) {
+            System.out.println("不能使用负数");
+            return;
+        }
+        for(int i = 1; i <= n; i++)
+            System.out.print(fib(i) + ", ");
+    }
 }
